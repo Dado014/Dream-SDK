365f6459c1e306679c9e9bfd4adc1192ffbfb1e9
Dado014 <137719349+Dado014@users.noreply.github.com>
2023-12-16 14:31:09 +0100

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,1 +1,5 @@
+const KV_URL = process.env.KV_URL
+const KV_REST_API_URL = process.env.KV_REST_API_URL
+const KV_REST_API_TOKEN = proccess.env.KV_REST_API_TOKEN
+const KV_REST_API_READ_ONLY_TOKEN = process.env.KV_REST_API_READ_ONLY_TOKEN
 
