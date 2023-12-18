abf6424c00de52a65eaddf5d1aaed4fb43667ae4
Dado014 <137719349+Dado014@users.noreply.github.com>
2023-12-18 14:08:43 +0100

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 443
+
 const KV_URL = process.env.KV_URL
 const KV_REST_API_URL = process.env.KV_REST_API_URL
-const KV_REST_API_TOKEN = proccess.env.KV_REST_API_TOKEN
+const KV_REST_API_TOKEN = process.env.KV_REST_API_TOKEN
 const KV_REST_API_READ_ONLY_TOKEN = process.env.KV_REST_API_READ_ONLY_TOKEN
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening to port ${PORT}`);
+  });
+  
